b906d3fb382ce4e9e3f792fd857d388c1ce64180
flodday <e.mpc41@gmail.com>
2024-02-29 15:21:11 +0100

    ajout langue + desktop de ma page

--- script1.js
+++ script1.js
@@ -13,7 +13,24 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   navMenu.classList.remove("active");
 }))
 
+/*Language selection*/ 
 
+const dropdown = document.querySelector('.drop-down');
+const list = document.querySelector('.list');
+const selected = document.querySelector('.selected');
+const selectedImg = document.querySelector('.selectedImg');
+
+dropdown.addEventListener('click', ()=>{
+    list.classList.toggle('show');
+})
+list.addEventListener('click', (e)=>{
+    const img = e.target.querySelector('img');
+    const text = e.target.querySelector('.text')
+
+    selectedImg.src = img.src;
+    selected.innerHTML= text.innerHTML;
+
+})
 /*Carrousel*/ 
 const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
