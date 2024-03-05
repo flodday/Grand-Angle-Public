0ae8850f5f81099a873007c77af00890fd23ee65
Hanekeko (Bilal Benyagoub) <75778663+kanekiadd@users.noreply.github.com>
2024-03-05 16:52:47 +0100

    commencement du php

---
+++ includes/pages/main.php
@@ -0,0 +1,4 @@
+
+    <?php
+    include "includes/pages/carousel1.php";
+    ?>
---
+++ includes/pages/nav-bar.php
@@ -0,0 +1,31 @@
+
+<div class="container">
+        <div class="subcontainer">
+          <nav class="navbar-container">
+            <div class="navbar-left">
+              <a href="#" class="nav-branding-1"
+                ><img src="assets/img/2 1.png" alt=""
+                class="image-GA"
+              /></a>
+              <div class="nav-menu">
+                <a href="#" class="nav-branding-2"
+                  ><img src="assets/img/2 1.png" alt=""
+                /></a>
+
+                <a href="#" class="nav-link">Expositions</a>
+                <a href="#" class="nav-link">Artistes</a>
+                <a href="#" class="nav-link">Collections</a>
+                <a href="#" class="nav-link">A propos</a>
+              </div>
+            </div>
+
+            <div class="navbar-right">
+              <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
